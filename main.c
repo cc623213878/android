37af631e04cf12d9b9afcea56f96da26470c45af
chch <15306070737@163.com>
2019-03-13 12:11:37 +0800

    0.1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+	实现一个AVL树算法，分别将23，2，7，10，3插入到树中
+	@author:陈春    软工一班
+	@date：2019.3.10   15:35 
+ */
+struct avlpoint{
+	int number;
+	int hight;
+	struct *left;
+	struct *right;
+};
+
+
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
